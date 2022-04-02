6057480d82e6105a347a45a4ab17ecc494879067
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2022-04-02 15:07:52 +0200

    some fixes in js code for more readability

--- index.js
+++ index.js
@@ -3,10 +3,45 @@
 let myForm      = document.getElementById("myForm");
 let rangeInput  = document.getElementById('rangeInput');
 let rangeSelect = document.getElementById('rangeSelect');
+let index = 0;
 
 // display range value when user move selector
 rangeSelect.textContent = rangeInput.value;
-rangeInput.oninput = function (){rangeSelect.textContent = rangeInput.value;}
+rangeInput.oninput = function ()
+{
+    rangeSelect.textContent = rangeInput.value;
+};
+
+//
+// FUNCTION CALLBACK collatzSyracuse(testNumber as integer)
+// while testNumber <> 1
+// if number is odd then testNumber / 2
+// else testNumber = testNumber * 3 + 1
+// 
+    
+function collatzSyracuse(testNumber)
+{
+    index++; // displayed the number the operation 
+    let p = document.createElement("p");
+
+    if (testNumber % 2 == 0 && testNumber !== 1)
+    {
+        p.innerHTML = "<b class='text-muted'>" + index + "# " + "</b>" + testNumber + " / 2 = " + testNumber / 2;
+        result.appendChild(p);
+        collatzSyracuse(testNumber / 2);
+    }
+    else if (testNumber % 2 == 1 && testNumber !== 1)
+    {
+        p.innerHTML= "<b class='text-muted'>" + index + "# " + "</b>" + "( " + testNumber + " * 3 ) + 1 = " + ((testNumber * 3) + 1);
+        result.appendChild(p);
+        collatzSyracuse((3 * testNumber) + 1);
+    }
+    return(testNumber);
+}
+
+//
+// on form submit calling function collatzSyracuse
+//
 
 myForm.addEventListener('submit', () =>
 {
@@ -18,14 +53,10 @@ myForm.addEventListener('submit', () =>
     result.setAttribute('id', 'result'); 
     body.appendChild(result);
 
-
     // when btn clear is clicked
     btn.addEventListener('click', (e)=> {
         result.remove();
     });
-
-    // set the number the operation 
-    let index       = 0;
 
     // set and displayed the range value
     let p = document.createElement("p");
@@ -36,37 +67,6 @@ myForm.addEventListener('submit', () =>
     // Calling the function to display results
     collatzSyracuse(testNumber);
     
-    //
-    // FUNCTION CALLBACK syracuseConjecture(integer)
-    // if number is odd and different to 1, then testNumber / 2
-    // else testNumber = testNumber * 3 + 1
-    // while testNumber <> 1
-    // 
-    function collatzSyracuse(testNumber)
-    {
-        index++; // displayed the number the operation 
-
-        if (testNumber % 2 == 0 && testNumber !== 1)
-        {
-            let p = document.createElement("p");
-            p.innerHTML = "<b class='text-muted'>" + index + "# " + "</b>" + testNumber + " / 2 = " + testNumber / 2;
-            result.appendChild(p);
-            collatzSyracuse(testNumber / 2);
-        }
-        else if (testNumber % 2 == 1 && testNumber !== 1)
-        {
-            let p = document.createElement("p");
-            p.innerHTML= "<b class='text-muted'>" + index + "# " + "</b>" + "( " + testNumber + " * 3 ) + 1 = " + ((testNumber * 3) + 1);
-            result.appendChild(p);
-            collatzSyracuse((3 * testNumber) + 1);
-        }
-        else if (testNumber == 1) 
-        {
-            console.log('fin de boucle');
-            return;
-        }
-    }
-
     //
     // FOOTER EXPLAIN RESULT
     // display result and some explanations when calcul is over
@@ -80,7 +80,4 @@ myForm.addEventListener('submit', () =>
     +" to infinity !";
     result.appendChild(p2);
 
-    // 
-    // END myForm.addEventListener('submit')
-    //
 });
